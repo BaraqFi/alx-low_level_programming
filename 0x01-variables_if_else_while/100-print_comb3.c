14ad256227f2c147f596e2ea48ee080871cf16ac
BaraqFi <mbarrack01@gmail.com>
2022-09-09 17:11:36 -0700

    to sleep

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Possible combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (i < 58)
+	{
+		e = i + 1;
+		while (e < 58)
+		{
+			putchar(i);
+			putchar(e);
+			if (i < 56 || e < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			e++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
