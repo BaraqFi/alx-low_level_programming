591546fa5b910468f82bb8dade2bd0d9dc509428
BaraqFi <mbarrack01@gmail.com>
2022-09-16 16:54:47 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: the number
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int c = 0;
+
+	while (c < n)
+	{
+		_putchar('_');
+		c++;
+	}
+	_putchar('\n');
+}
