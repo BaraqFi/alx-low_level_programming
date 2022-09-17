d4c2fdd4e3d24b4e7479cc3b7a1955663f7d6664
BaraqFi <mbarrack01@gmail.com>
2022-09-16 17:06:28 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - prints the largest number
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int prime = 612852475143;
+	long int q = prime;
+	long int d = 2;
+
+	while (q != d)
+	{
+		if (q % d == 0)
+		{
+			q = q / d;
+		}
+		else
+		{
+			d++;
+		}
+	}
+	printf("%li\n", q);
+	return (0);
+}
